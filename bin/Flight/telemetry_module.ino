f4d3fd25fc9921c2b729880f5f5676fd4c8c1eb8
Roux Heyns <rheyns@gmail.com>
2015-06-10 21:27:11 -0700

    Re-add row-telemetry print out

--- bin/Flight/telemetry_module.ino
+++ bin/Flight/telemetry_module.ino
@@ -429,6 +429,35 @@ void process_telemetry()
 void print_telemetry()
 {
   Serial.println("-----------Telemetry---------------");
+  Serial.print("raw Air Pressure: ");
+  raw_val = analogRead(PIN_PRESSURE_SENSOR);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.print("raw Battery 1-1 Temp: ");
+  raw_val = analogRead(PIN_BATTERY1_1_TEMP);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.print("raw Battery 1-2 Temp: ");
+  raw_val = analogRead(PIN_BATTERY1_2_TEMP);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.print("raw Battery 2-1 Temp: ");
+  raw_val = analogRead(PIN_BATTERY2_1_TEMP);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.print("raw Battery 2-2 Temp: ");
+  raw_val = analogRead(PIN_BATTERY2_2_TEMP);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.print("raw Outter External Temp: ");
+  raw_val = analogRead(PIN_EXTERNAL_INNER_TEMP);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.print("raw Inner External Temp: ");
+  raw_val = analogRead(PIN_EXTERNAL_OUTTER_TEMP);
+  delay(100);
+  Serial.println(raw_val);
+  Serial.println("--------------------------------");
   Serial.print("Air Pressure: ");
   Serial.println(telemetry_data.air_pressure);
   Serial.print("Battery 1-1 Temp: ");
