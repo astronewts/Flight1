406f38659f3c1b81aa833d5325a8b9cc1472dd24
Kevin Macko <kevinmacko@yahoo.com>
2016-01-19 22:34:14 -0800

    Updated camera module to fix a potential error with camera on time reset

--- bin/Flight/camera_module.ino
+++ bin/Flight/camera_module.ino
@@ -1,13 +1,13 @@
 
 void process_camera_function()
 {
-   //Is it time to turn camera on/off?
+   //Is the camera enabled for the current mode?
    if(parameters.camera_enabled == true)
    {
       //Is the camera on?
-      // TODO: FIX THIS !!!
       if(parameters.camera_status == true)
       {
+         //Is it time to turn camera off?
          if(parameters.camera_on_elapsed_time > parameters.camera_on_time)
          {
             //Turn Camera Off
@@ -20,14 +20,16 @@ void process_camera_function()
       }
       else
       {
+         //Reset the camera on elapsed time
+         parameters.camera_on_elapsed_time = 0;
+         
+         //Is it time to turn camera on?
          if(parameters.camera_period_elapsed_time > (parameters.camera_period - parameters.camera_on_time))
          {
             //Turn Camera On
             digitalWrite(PIN_CAMERA_SWITCH, HIGH);
             parameters.camera_status = true;
             
-            //Reset the camera on elapsed time
-            parameters.camera_on_elapsed_time = 0;
           }
       }  
    }
