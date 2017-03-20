ed4450ed61f769ed426597dbbd05ca0771b15cfe
nicocranfield <nico_cranfield@yahoo.fr>
2017-03-19 18:30:54 -0700

    Update database, step2 for figures and period for RB

--- Read_SD_and_RB_data/Step2_Create_figures_from_mat_workspace.m
+++ Read_SD_and_RB_data/Step2_Create_figures_from_mat_workspace.m
@@ -1,22 +1,47 @@
 %% Create figures
-figure(1)
-line(Elapsed_Time_ms_SD/1000,Heat_State_1_SD*30,'Color',[0 0 0])
-line(Elapsed_Time_ms_SD/1000,(B1_T_1_C_SD+B1_T_2_C_SD)/2,'Color',[0 0.5 0])
-line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_Low_K_SD,'Color',[1 0 0])
-line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_High_K_SD,'Color',[1 0.8 0])
-line(Elapsed_Time_ms_SD/1000,10+Veh_Mode_SD,'Color',[0 0 0.5])
-ylim([10 32])
-%
-figure(2)
-line(Elapsed_Time_ms_SD/1000,Heat_State_2_SD*30,'Color',[0 0 0])
-line(Elapsed_Time_ms_SD/1000,(B2_T_1_C_SD+B2_T_2_C_SD)/2,'Color',[0 0.5 0])
-line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_Low_K_SD,'Color',[1 0 0])
-line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_High_K_SD,'Color',[1 0.8 0])
-line(Elapsed_Time_ms_SD/1000,10+Veh_Mode_SD,'Color',[0 0 0.5])
-ylim([10 32])
 % figure(1)
+% line(Elapsed_Time_ms_SD/1000,Heat_State_1_SD*30,'Color',[0 0 0])
+% line(Elapsed_Time_ms_SD/1000,(B1_T_1_C_SD+B1_T_2_C_SD)/2,'Color',[0 0.5 0])
+% line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_Low_K_SD,'Color',[1 0 0])
+% line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_High_K_SD,'Color',[1 0.8 0])
+% line(Elapsed_Time_ms_SD/1000,10+Veh_Mode_SD,'Color',[0 0 0.5])
+% ylim([10 32])
+% %
+% figure(2)
+% line(Elapsed_Time_ms_SD/1000,Heat_State_2_SD*30,'Color',[0 0 0])
+% line(Elapsed_Time_ms_SD/1000,(B2_T_1_C_SD+B2_T_2_C_SD)/2,'Color',[0 0.5 0])
+% line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_Low_K_SD,'Color',[1 0 0])
+% line(Elapsed_Time_ms_SD/1000,B_Active_T_Lim_High_K_SD,'Color',[1 0.8 0])
+% line(Elapsed_Time_ms_SD/1000,10+Veh_Mode_SD,'Color',[0 0 0.5])
+% ylim([10 32])
+% close all
+% figure(1)
+% line(Elapsed_Time_ms_SD/1000,Alt_m_SD,'Marker','o')
+% line(Elapsed_Time_ms_SD/1000,GPS_Alt_m_SD,'Color',[1 0 0],'Marker','+')
+% grid on
+% legend([{'Altimeter'};{'GPS'}],'Location','NorthWest')
+% xlabel('Time [s]')
+% ylabel('Altitude [m]')
+% saveas(gcf,'Altitude.png','png')
+% close(gcf)
+% %
+% figure(2)
 % line(Elapsed_Time_ms_SD/1000,Veh_Mode_SD)
-% line(Elapsed_Time_s_RB,Veh_Mode_RB,'Color',[1 0 0],'Marker','+','LineStyle','none')
+% grid on
+% xlabel('Time [s]')
+% ylabel('Veh mode')
+% saveas(gcf,'Veh_mode.png','png')
+% close(gcf)
+% %
+% figure(3)
+% line(GPS_Long_deg_SD,GPS_Lat_deg_SD)
+% grid on
+% ylim([37 40])
+% xlim([-123 -119])
+% xlabel('Longitude [deg]')
+% ylabel('Lattitude [deg]')
+% saveas(gcf,'Lat_long.png','png')
+% close(gcf)
 % %
 % figure(2)
 % line(Elapsed_Time_ms_SD/1000,Cam_Status_SD)
@@ -46,29 +71,29 @@ ylim([10 32])
 % line(Elapsed_Time_s_RB,Max_GPS_Alt_m_RB,'Color',[1 0 0],'Marker','+','LineStyle','none')
 % line(Elapsed_Time_s_RB,Min_GPS_Alt_m_RB,'Color',[0 0.5 0],'Marker','+','LineStyle','none')
 % line(Elapsed_Time_s_RB,Mean_GPS_Alt_m_RB,'Color',[0 0 0],'Marker','v','LineStyle','none')
-% % %%
-% close all
-% Read_Database_from_Arduino
-% clearvars -except *_RB *_SD *var_name
-% for v=131:140%size(var_name,2)
-%     try
-% Y_SD=evalin('base',strcat(char(var_name(1,v)),'_SD'));
-%     end
-%     try
-% Y_RB=evalin('base',strcat(char(var_name(1,v)),'_RB'));
-%     end
-% %
-% if (exist('Y_SD')==1 && exist('Y_RB')==1) 
-% h=figure(v);
-% set(gcf,'Visible','on')
-% line(Elapsed_Time_ms_SD/1000,Y_SD)
-% line(Elapsed_Time_s_RB,Y_RB,'Color',[1 0 0],'Marker','+','LineStyle','none')
-% xlabel('Time [s]')
-% ylabel(strrep(char(var_name(1,v)),'_','-'))
-% grid on
-% pause(2)
-% clear Y_RB Y_SD
-% else
-% display(strcat(char(var_name(1,v)),'=> DOES NOT EXIST'))    
-% end
-%end
+%%
+close all
+Read_Database_from_Arduino
+clearvars -except *_RB *_SD *var_name
+for v=41:60%size(var_name,2)
+    try
+Y_SD=evalin('base',strcat(char(var_name(1,v)),'_SD'));
+    end
+    try
+Y_RB=evalin('base',strcat(char(var_name(1,v)),'_RB'));
+    end
+%
+if (exist('Y_SD')==1 )%&& exist('Y_RB')==1) 
+h=figure(v);
+set(gcf,'Visible','on')
+line(Elapsed_Time_ms_SD/1000,Y_SD)
+%line(Elapsed_Time_s_RB,Y_RB,'Color',[1 0 0],'Marker','+','LineStyle','none')
+xlabel('Time [s]')
+ylabel(strrep(char(var_name(1,v)),'_','-'))
+%xlim([3000 6000])
+grid on
+clear Y_RB Y_SD
+else
+display(strcat(char(var_name(1,v)),'=> DOES NOT EXIST'))    
+end
+end
